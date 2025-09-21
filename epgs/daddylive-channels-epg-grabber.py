3b1e54412ee1e72ecc556a3c99696c6112b195a2
sakidahjumaat <jemremy82@gmail.com>
2025-09-21 08:47:28 +0800

    Update daddylive-channels-epg-grabber.py

--- epgs/daddylive-channels-epg-grabber.py
+++ epgs/daddylive-channels-epg-grabber.py
@@ -112,6 +112,7 @@ urls = [
     "https://epgshare01.online/epgshare01/epg_ripper_TR1.xml.gz",
     "https://epgshare01.online/epgshare01/epg_ripper_UK1.xml.gz",
     "https://epgshare01.online/epgshare01/epg_ripper_US1.xml.gz",
+    "https://viplink.biz/epgg.xml",
     "https://epgshare01.online/epgshare01/epg_ripper_US_LOCALS2.xml.gz",
     "https://epgshare01.online/epgshare01/epg_ripper_UY1.xml.gz",
     "https://epgshare01.online/epgshare01/epg_ripper_ZA1.xml.gz",
