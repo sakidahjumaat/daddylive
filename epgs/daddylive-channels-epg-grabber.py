dbd1eebef1d50eb52c55b15d9f5feb0e873d0340
sakidahjumaat <jemremy82@gmail.com>
2026-04-29 09:13:29 +0800

    Update daddylive-channels-epg-grabber.py

--- epgs/daddylive-channels-epg-grabber.py
+++ epgs/daddylive-channels-epg-grabber.py
@@ -138,6 +138,7 @@ urls = [
     "https://www.open-epg.com/files/unitedkingdom1.xml",
     "https://www.open-epg.com/files/unitedstates1.xml.gz",
     "https://www.open-epg.com/files/unitedstates10.xml.gz",
+    "https://www.open-epg.com/files/india1.xml",
     "https://www.open-epg.com/files/unitedstates11.xml.gz",
 ]
 
